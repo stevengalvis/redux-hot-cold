1f614210c5a4fb0d7b62336b917d2a0aebd1937c
stevengalvis <steven.galvis@gmail.com>
2017-10-04 17:54:33 -0400

    added test for makeGuess

--- src/actions.test.js
+++ src/actions.test.js
@@ -6,3 +6,13 @@ describe('newGame', () => {
     expect(action.type).toEqual(NEW_GAME);
   });
 });
+
+
+describe('makeGuess', () => {
+  it('Should return the action', () => {
+    const guess = 5
+    const action = makeGuess(guess);
+    expect(action.type).toEqual(MAKE_GUESS);
+    expect(action.guess).toEqual(guess);
+  })
+})
